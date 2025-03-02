99b15ea63061fbac5f0100a0edba52df505f8d66
Marcelo <marceloamaral1364@gmail.com>
2025-03-02 16:42:56 -0300

    Setting .env variables, database and debug urls

--- djangoapp/project/urls.py
+++ djangoapp/project/urls.py
@@ -16,7 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+
+# Ver os arquivos que foram subidos pelos usuários mesmo em modo de debug
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
