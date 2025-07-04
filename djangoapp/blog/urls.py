a68cf327657463206bb1d33bf2d92eb892fad67b
Marcelo <marceloamaral1364@gmail.com>
2025-07-03 22:45:40 -0300

    Creating HTML and CSS base and layout

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -4,6 +4,8 @@ from blog import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('page', views.page, name='page'),
+    path('post', views.post, name='post')
 ]
 
