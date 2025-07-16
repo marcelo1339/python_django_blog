ac4d356d98d548cc0136c7ef5440eb487bbb9e78
Marcelo <marceloamaral1364@gmail.com>
2025-07-15 22:07:38 -0300

    Changing Model Post and adding Customization to Post-Card

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -89,14 +89,21 @@ class Page(models.Model):
         return self.title
 
 
+class PostManager(models.Manager):
+    def get_published(self):
+        return self.filter(is_published=True).order_by('-pk')
+
+
 class Post(models.Model):
     class Meta:
         verbose_name = 'Post'
         verbose_name_plural = 'Posts'
 
+    objects = PostManager()
+
     title = models.CharField(max_length=65,)
     slug = models.SlugField(unique=True, default='', null=False, blank=True, max_length=255)
-    exerpt = models.CharField(max_length=150)
+    excerpt = models.CharField(max_length=150)
     is_published = models.BooleanField(
         default=False,
         help_text='Marque para o post ficar visível públicamente.'
