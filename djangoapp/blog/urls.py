825535430524119272536d4fbda9f529b7284ea8
Marcelo <marceloamaral1364@gmail.com>
2025-07-16 18:21:16 -0300

    Creating category and created by views. And rendering post

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -5,7 +5,9 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('page', views.page, name='page'),
-    path('post/<slug:slug>/', views.post, name='post')
+    path('page/<slug:slug>', views.page, name='page'),
+    path('post/<slug:slug>/', views.post, name='post'),
+    path('created_by/<int:author_pk>/', views.created_by, name='created_by'),
+    path('category/<slug:slug>/', views.category, name='category')
 ]
 
