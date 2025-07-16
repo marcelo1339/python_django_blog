4734f205ee8fe34823cfa87d45472cbc1d8e3efc
Marcelo <marceloamaral1364@gmail.com>
2025-07-15 23:09:47 -0300

    Adding get_absolute_url to Post

--- djangoapp/blog/admin.py
+++ djangoapp/blog/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Tag, Category, Page, Post
 from django_summernote.admin import SummernoteModelAdmin
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 
 # Register your models here.
 @admin.register(Tag)
@@ -45,7 +47,6 @@ class PageAdmin(SummernoteModelAdmin):
 class PostAdmin(SummernoteModelAdmin):
 
     summernote_fields = 'content',
-
     list_display = 'id', 'title', 'is_published', 'created_by',
     list_display_links = 'title',
     search_fields = 'id', 'slug', 'title', 'excerpt', 'content',
@@ -53,11 +54,18 @@ class PostAdmin(SummernoteModelAdmin):
     list_filter = 'category', 'is_published',
     list_editable = 'is_published',
     ordering = '-id',
-    readonly_fields = 'created_at', 'updated_at', 'updated_by', 'created_by',
+    readonly_fields = 'created_at', 'updated_at', 'updated_by', 'created_by', 'link'
     prepopulated_fields = {
         'slug': ['title',]
     }
     autocomplete_fields = 'tags', 'category',
+    
+    def link(self, obj):
+
+        url_post = obj.get_absolute_url()
+        safe_link = mark_safe(f'<a href="{url_post}" target="_blank" >Ver post</a>')
+
+        return safe_link
     
     def save_model(self, request, obj, form, change):
 
