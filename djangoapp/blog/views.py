1e3b19619e9e074ab1051f605780d16afdca8154
Marcelo <marceloamaral1364@gmail.com>
2025-07-17 18:44:03 -0300

    feat: Adding search

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,6 +1,7 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render
 from blog.models import Post
+from django.db.models import Q
 
 PER_PAGE = 9
 
@@ -93,5 +94,25 @@ def post(request, slug):
         {
             # 'page_obj': page_obj,
             'post': requested_post
+        }
+    )
+
+def search(request):
+    search_value = request.GET.get('search', '').strip()
+    posts = (
+        Post.objects.get_published()
+        .filter(
+            Q(title__icontains=search_value) |
+            Q(excerpt__icontains=search_value) |
+            Q(content__icontains=search_value) 
+        )[:PER_PAGE]
+    )
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': posts,
+            'search_value': search_value
         }
     )
