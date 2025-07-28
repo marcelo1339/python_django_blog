11a771a22b483426ba79482f8a0f878b93bab4d4
Marcelo <marceloamaral1364@gmail.com>
2025-07-28 10:12:53 -0300

    Feat: Changing Page and Post to CBVs

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,10 +1,9 @@
-from django.core.paginator import Paginator
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
 from blog.models import Post, Page
 from django.db.models import Q
 from django.contrib.auth.models import User
 from django.http import Http404
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 
 PER_PAGE = 9
@@ -75,34 +74,6 @@ class CreatedByListView(PostListView):
         return super().get(request, *args, **kwargs)
     
 
-def created_by(request, author_pk):
-
-    author = User.objects.filter(pk=author_pk).first()
-
-    if author is None:
-        raise Http404()
-
-    author_full_name = author.username
-
-    if author.first_name:
-        author_full_name = f'{author.first_name} {author.last_name}'
-
-    page_title = f'Posts de {author_full_name} - '
-
-    posts = Post.objects.get_published().filter(created_by__pk=author_pk)     
-
-    paginator = Paginator(posts, PER_PAGE)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
-
-    return render(
-        request,
-        'blog/pages/index.html',
-        {
-            'page_obj': page_obj,
-            'page_title': page_title,
-        }
-    )
 
 class CategoryListView(PostListView):
     allow_empty = False
@@ -140,47 +111,52 @@ class TagListView(PostListView):
 
         return ctx
 
+class PageDetailView(DetailView):
+    model = Page
+    template_name = 'blog/pages/page.html'
+    slug_field = 'slug'
+    context_object_name = 'page'
 
-def page(request, slug):
+    def get_context_data(self, *args, **kwargs):
+        
+        ctx = super().get_context_data(*args, **kwargs)
+        page = self.get_object()
 
-    requested_page = Page.objects.get_published().filter(slug=slug).first()
+        page_title = f'{page.title} - Página - '
 
-    if requested_page is None:
-        raise Http404()
-
-    page_title = f'{requested_page.title} - Página - '
-    return render(
-        request,
-        'blog/pages/page.html',
-        {
-            'page': requested_page,
+        ctx.update({
             'page_title': page_title,
-        }
-    )
+        })
 
-
-def post(request, slug):
-
-    requested_post = (
-        Post.objects
-        .get_published()
-        .filter(slug=slug)
-        .first()
-    )
-
-    if requested_post is None:
-        raise Http404()
+        return ctx
     
-    page_title = f'{requested_post.title} - Post - '
+    def get_queryset(self):
 
-    return render(
-        request,
-        'blog/pages/post.html',
-        {
-           'post': requested_post,
+        return super().get_queryset().filter(is_published=True)
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/pages/post.html'
+    slug_field = 'slug'
+    context_object_name = 'post'
+
+    def get_queryset(self):
+        qs = super().get_queryset().filter(is_published=True)
+        return qs
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+
+        post = self.get_object()
+        
+        page_title = f'{post.title} - Post - '
+
+        ctx.update({
             'page_title': page_title,
-        }
-    )
+        })
+
+        return ctx
 
 
 class SearchListView(PostListView):
@@ -225,26 +201,3 @@ class SearchListView(PostListView):
             return redirect('blog:index')
 
         return super().get(request, *args, **kwargs)
-
-def search(request):
-    search_value = request.GET.get('search', '').strip()
-    posts = (
-        Post.objects.get_published()
-        .filter(
-            Q(title__icontains=search_value) |
-            Q(excerpt__icontains=search_value) |
-            Q(content__icontains=search_value) 
-        )[:PER_PAGE]
-    )
-
-    page_title = f'{search_value[:30]} - Search - '
-
-    return render(
-        request,
-        'blog/pages/index.html',
-        {
-            'page_obj': posts,
-            'search_value': search_value,
-            'page_title': page_title,
-        }
-    )
